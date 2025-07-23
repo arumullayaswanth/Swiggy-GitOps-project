3fa5691f61f80b4f9bac5e1c060ef70947ff6bee
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-07-23 16:07:02 +0530

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "hotstaarumullaa"
+  bucket = "swiggyarumullaa"
 
   tags = {
-    Name        = "hotstaarumullaa"
+    Name        = "swiggyarumullaa"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "hotstaaluru"
+  bucket = "swiggyaluru"
 
   tags = {
-    Name        = "hotstaaluru"
+    Name        = "swiggyaluru"
     Environment = "dev"
   }
 }
